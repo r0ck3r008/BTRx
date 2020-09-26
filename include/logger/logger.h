d2ea02ba716bc5e3a6a2fda774028bdd8952f1bc
r0ck3r008 <naman.arora@tutanota.com>
2020-09-26 19:55:31 -0400

    Divided enums into two parts LOG_LVL and LOG_TYPE
    	The LOG_LVL is the first filter which decides if the current set
    	max level is higher or equal to the incoming request level if it
    	is the request is further processed.
    	Also, this gives leverage in how high verbosity can the
    	application have, has four levels with the LOG_INFO level
    	divided into 11 seprate parts.

--- include/logger/logger.h
+++ include/logger/logger.h
@@ -5,21 +5,26 @@
 
 using std::string;
 
-typedef enum LOG_LVL {
-	LOG_DBG = 1 << 1,
-	LOG_CXNT = 1 << 2,
-	LOG_CXNR = 1 << 3,
-	LOG_NBR = 1 << 4,
-	LOG_OUCHK = 1 << 5,
-	LOG_UCHK = 1 << 6,
-	LOG_CHK = 1 << 7,
-	LOG_HV = 1 << 8,
-	LOG_INT = 1 << 9,
-	LOG_UINT = 1 << 10,
-	LOG_DWL = 1 << 11,
-	LOG_DWLD = 1 << 12,
-	LOG_ERR = 1 << 13
+typedef enum LOG_LVL: uint16_t {
+	LOG_DBG = 1 << 12,
+	LOG_INFO = 1 << 13,
+	LOG_WRN = 1 << 14,
+	LOG_ERR = 1 << 15
 } LOG_LVL;
+
+typedef enum LOG_TYPE: uint16_t {
+	LOG_CXNT = (1 << 1) | LOG_WRN,
+	LOG_CXNR = (1 << 2) | LOG_WRN,
+	LOG_NBR = (1 << 3) | LOG_WRN,
+	LOG_OUCHK = (1 << 4) | LOG_WRN,
+	LOG_UCHK = (1 << 5) | LOG_WRN,
+	LOG_CHK = (1 << 6) | LOG_WRN,
+	LOG_HV = (1 << 7) | LOG_WRN,
+	LOG_INT = (1 << 8) | LOG_WRN,
+	LOG_UINT = (1 << 9) | LOG_WRN,
+	LOG_DWL = (1 << 10) | LOG_WRN,
+	LOG_DWLD = (1 << 11) | LOG_WRN,
+} LOG_TYPE;
 
 class Logger {
 	int sock;
