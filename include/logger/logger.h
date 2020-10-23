6cdcd01ddd47be0c6b71f500957dcb3540f4b29c
r0ck3r008 <naman.arora@tutanota.com>
2020-10-23 01:36:28 -0400

    Refer commit 4bec7b2.

--- include/logger/logger.h
+++ include/logger/logger.h
@@ -3,36 +3,19 @@
 
 #include<stdarg.h>
 
+#include"logger/logmsg.pb.h"
+
 using std::string;
 
-typedef enum LOG_LVL: uint16_t {
-	LOG_DBG = 1 << 12,
-	LOG_INFO = 1 << 13,
-	LOG_WRN = 1 << 14,
-	LOG_ERR = 1 << 15
-} LOG_LVL;
-
-typedef enum LOG_TYPE: uint16_t {
-	LOG_CXNT = (1 << 1) | LOG_WRN,
-	LOG_CXNR = (1 << 2) | LOG_WRN,
-	LOG_NBR = (1 << 3) | LOG_WRN,
-	LOG_OUCHK = (1 << 4) | LOG_WRN,
-	LOG_UCHK = (1 << 5) | LOG_WRN,
-	LOG_CHK = (1 << 6) | LOG_WRN,
-	LOG_HV = (1 << 7) | LOG_WRN,
-	LOG_INT = (1 << 8) | LOG_WRN,
-	LOG_UINT = (1 << 9) | LOG_WRN,
-	LOG_DWL = (1 << 10) | LOG_WRN,
-	LOG_DWLD = (1 << 11) | LOG_WRN,
-} LOG_TYPE;
-
-class Logger {
-	int sock;
-	LOG_LVL max_lvl;
-public:
-	Logger(string, LOG_LVL);
-	~Logger();
-	void write_msg(LOG_LVL, int *, int);
-};
+namespace logger {
+	class Logger {
+		int sock;
+		LogLvlT max_lvl;
+	public:
+		Logger(string, LogLvlT);
+		~Logger();
+		void write_msg(LogLvlT, int *, int);
+	};
+}
 
 #endif
