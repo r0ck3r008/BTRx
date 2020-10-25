b317b70f48a34dabbb8c73d9cae20c3fb42e1ebf
r0ck3r008 <naman.arora@tutanota.com>
2020-10-25 02:11:04 -0400

    Function to read in conf file

--- node/entrypoint.cpp
+++ node/entrypoint.cpp
@@ -33,6 +33,32 @@ void read_peer_info(string& fname, vector<char *>& peers)
 	}
 }
 
+void read_conf_file(string& fname, string& sh_fname, vector<int>& vals)
+{
+	FILE *f = NULL;
+	if((f = fopen(fname.c_str(), "r")) == NULL) {
+		lvar->write_msg(LogMsgT::LOG_ERR, "ENTRYPOINT: Fopen: %s",
+					fname.c_str());
+		_exit(1);
+	}
+
+	char *line = NULL;
+	size_t n = 0;
+	char tmp[512];
+	while(getline(&line, &n, f) > 0) {
+		strncpy(tmp, line, 512 * sizeof(char));
+		char *field = strtok(tmp, " ");
+		if(!strcmp(field, "FileName"))
+			sh_fname = string(strtok(NULL, " "));
+		else
+			vals.push_back(strtol(strtok(NULL, " "), NULL, 10));
+
+		free(line);
+		line = NULL;
+		n = 0;
+	}
+}
+
 int main(int argc, char **argv)
 {
 	if(argc != 2) {
@@ -42,6 +68,11 @@ int main(int argc, char **argv)
 
 	string fname = "./log_peer_" + string(argv[1]);
 	lvar = new Logger(fname, LogMsgT::LOG_DBG);
+
+	vector<int> vals;
+	string sh_fname;
+	fname = "./Common.cfg";
+	read_conf_file(fname, sh_fname, vals);
 
 	vector<char *> peers;
 	fname = "./peer_" + string(argv[1]) + "/" + "PeerInfo.cfg";
