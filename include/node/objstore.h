b80c92932ed1fbdedd21a971a37e5541f5a6b2b8
r0ck3r008 <naman.arora@tutanota.com>
2020-10-25 14:57:48 -0400

    Merging the BitField class to ObjStore
    	This change is important as the access functions for the
    	bitfield would inevitably need some gateway within the objstore
    	which would then be un-necessary. Thus, making bitfield an
    	attribute of ObjStore.

--- include/node/objstore.h
+++ include/node/objstore.h
@@ -3,9 +3,6 @@
 
 #include<vector>
 
-#include"node/bitfield.h"
-
-using node::BitField;
 using std::vector;
 using std::string;
 
@@ -15,11 +12,16 @@ namespace node {
 		int pcsz;
 		int npcs;
 		string fname;
-		BitField *bfield;
+	public:
+		vector<uint64_t> bfield;
 	public:
 		ObjStore(int, int, string);
 		~ObjStore();
 		void bfield_init(bool);
+		void bfield_diff(vector<uint64_t>&,
+				vector<uint64_t>&);
+		bool bfield_exists(int);
+		void bfield_flip(int);
 	};
 }
 
--- node/objstore.cpp
+++ node/objstore.cpp
@@ -5,13 +5,11 @@
 #include<unistd.h>
 
 #include"logger/logger.h"
-#include"node/bitfield.h"
 #include"node/objstore.h"
 
 using logger::Logger;
 using logger::LogMsgT;
 using node::ObjStore;
-using node::BitField;
 using std::vector;
 using std::string;
 
@@ -25,13 +23,60 @@ ObjStore :: ObjStore(int fsz, int pcsz, string fname)
 	this->fname = fname;
 }
 
-void ObjStore :: bfield_init(bool hasfile)
-{
-	this->bfield = new BitField(hasfile, this->npcs, this->fsz,
-				this->fname.c_str());
-}
-
 ObjStore :: ~ObjStore()
 {
-	delete this->bfield;
+}
+
+void ObjStore :: bfield_init(bool hasfile)
+{
+	if(hasfile) {
+		int wholes = this->npcs / 64;
+		int lftovr = this->npcs % 64;
+		/*TODO:
+		*Make sure that memory allocated for this vector
+		*Doesn't go out of scope once this function ends.
+		*/
+		this->bfield = vector<uint64_t>(wholes, 1);
+		if(lftovr) {
+			uint64_t mask = 0;
+			int max = (64 - lftovr);
+			for(int i=63; i>=max; i--)
+				mask ^ ((uint64_t)1<<i);
+			this->bfield.push_back(mask);
+		}
+	} else {
+		char cmd[512];
+		sprintf(cmd, "/usr/bin/head -c %d /dev/zero > %s",
+					this->fsz, this->fname.c_str());
+		system(cmd);
+	}
+}
+
+void ObjStore :: bfield_diff(vector<uint64_t>& right, vector<uint64_t>& diff)
+{
+	vector<uint64_t> left = this->bfield;
+	for(int i=0; i<left.size(); i++) {
+		uint64_t mask = left[i] ^ (left[i] | right[i]);
+		diff.push_back(mask);
+	}
+}
+
+bool ObjStore :: bfield_exists(int pos)
+{
+	/* TODO: Make absolutely sure this method works */
+	int wholes = pos / 64;
+	int lftovr = pos % 64;
+	uint64_t mask = 1<<lftovr;
+	if(this->bfield[wholes] & mask)
+		return true;
+	else
+		return false;
+}
+
+void ObjStore :: bfield_flip(int pos)
+{
+	int wholes = pos / 64;
+	int lftovr = pos % 64;
+	uint64_t mask = 1<<lftovr;
+	this->bfield[wholes] ^= mask;
 }
