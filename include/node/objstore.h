9273272b95a1b3a99874ddf7c36b6a5fd29f5ddb
r0ck3r008 <naman.arora@tutanota.com>
2020-10-25 05:14:31 -0400

    Object store template, Doesn't compile

---
+++ include/node/objstore.h
@@ -0,0 +1,26 @@
+#ifndef OBJSTORE_H
+#define OBJSTORE_H
+
+#include<vector>
+
+#include"node/bitfield.h"
+
+using node::BitField;
+using std::vector;
+using std::string;
+
+namespace node {
+	class ObjStore {
+		int fsz;
+		int pcsz;
+		int npcs;
+		string fname;
+		BitField bfield;
+	public:
+		ObjStore(int, int, string);
+		~ObjStore();
+		void bfield_init(bool);
+	};
+}
+
+#endif
---
+++ node/objstore.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<vector>
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+
+#include"logger/logger.h"
+#include"node/bitfield.h"
+#include"node/objstore.h"
+
+using logger::Logger;
+using logger::LogMsgT;
+using node::ObjStore;
+using node::BitField;
+using std::vector;
+using std::string;
+
+extern Logger *lvar;
+
+ObjStore :: ObjStore(int fsz, int pcsz, string fname)
+{
+	this->fsz = fsz;
+	this->pcsz = pcsz;
+	this->npcs = (fsz % pcsz) ? ((fsz/pcsz) + 1) : (fsz/pcsz);
+	this->fname = fname;
+}
+
+void ObjStore :: bfield_init(bool hasfile)
+{
+	this->bfield = BitField(hasfile, this->npcs, this->fsz,
+				this->fname.c_str());
+}
+
+ObjStore :: ~ObjStore() { }
