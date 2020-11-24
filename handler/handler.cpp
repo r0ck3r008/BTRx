57f8f0d223d691cdbba9ec5c7c0bab703668e7c3
r0ck3r008 <naman.arora@tutanota.com>
2020-11-24 03:15:31 -0500

    Call the threads for sendloop and recvloops

--- handler/handler.cpp
+++ handler/handler.cpp
@@ -7,11 +7,14 @@
 #include<errno.h>
 
 #include"logger/logger.h"
+#include"handler/sendloop.h"
+#include"handler/recvloop.h"
 #include"handler/handler.h"
 
 using std::cout;
 using std::endl;
 using logger::Logger;
+using logger::LogLvlT;
 using namespace handler;
 
 extern Logger *lvar;
@@ -32,12 +35,35 @@ HandlerArgs :: ~HandlerArgs()
 void *cli_handler(void *args)
 {
         HandlerArgs *harg = (HandlerArgs *)args;
+        /* TODO: Replace this with appropriate logger statements */
         if(harg->client)
                 cout << "Connection accepted from: ";
         else
                 cout << "Connected to: ";
 
         cout << inet_ntoa(harg->addr->sin_addr) << endl;
+
+        pthread_t tid[2];
+        int stat = 0;
+        if((stat = pthread_create(&tid[0], NULL, sendloop, args)) != 0) {
+                lvar->write_msg(LogLvlT::LOG_ERR, "NODE: Create: %s\n",
+                                strerror(stat));
+                _exit(1);
+        }
+
+        if((stat = pthread_create(&tid[0], NULL, recvloop, args)) != 0) {
+                lvar->write_msg(LogLvlT::LOG_ERR, "NODE: Create: %s\n",
+                                strerror(stat));
+                _exit(1);
+        }
+
+        for(int i=0; i<2; i++) {
+                if((stat = pthread_join(tid[i], NULL)) != 0) {
+                        lvar->write_msg(LogLvlT::LOG_ERR, "NODE: Join: %s\n",
+                                        strerror(stat));
+                        _exit(1);
+                }
+        }
 
         pthread_exit(NULL);
 }
