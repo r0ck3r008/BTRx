22b718fb59ae700ee1c379920e0dd2f127bbf3b2
r0ck3r008 <naman.arora@tutanota.com>
2020-11-24 01:55:01 -0500

    Updated implementation of update_cache
            1. The update cache now checks if the incoming element is
            already present. In case it is, it just pushes it to most
            recently seen state from wherever it is in the array.
            2. Compliance with commit e0d0b4c

--- objstore/cache.cpp
+++ objstore/cache.cpp
@@ -68,20 +68,32 @@ int Cache :: get_pos(int pcno)
 
 void Cache :: update_cache(int pos, char *buf)
 {
-	auto itr = this->cvec.end();
-	Access *acc = new Access(pos, buf),
-		*acc_old = *itr;
-	if(this->cvec.size() == this->maxsz) {
-		/* erase Least Freq used */
-		this->cvec.erase(itr);
-		auto itr_map = this->cmap.find(acc_old->pos);
-		this->cmap.erase(itr_map);
-		delete acc_old;
-	}
-	/* Insert new Access */
-	this->cvec.push_front(acc);
-	auto itr_new = this->cvec.begin();
-	this->cmap.insert(pair<int, deque<Access *>::iterator>(pos, itr_new));
+        auto itr = this->cmap.find(pos);
+        if(itr != this->cmap.end()) {
+                Access *old_acc = this->cvec.front();
+                /* Already exists */
+		/* Remove from current position */
+		this->cvec.erase(this->cvec.begin() + itr->second);
+		this->cmap.erase(itr);
+		/* Push to back */
+		this->cmap.insert(pair<int, int>(pos, this->cvec.size()));
+		this->cvec.push_back(old_acc);
+        } else {
+                // Not found
+                Access *new_acc = new Access(pos, buf);
+                if(new_acc == NULL) {
+                        lvar->write_msg(LogLvlT::LOG_ERR, "CACHE: Malloc");
+                        _exit(1);
+                }
+                if(this->cvec.size() == this->maxsz) {
+                        // Evict the least used
+                        Access *old_acc = this->cvec.front();
+                        this->cvec.pop_front();
+                        this->cmap.erase(old_acc->pos);
+                }
+                this->cmap.insert(pair<int, int>(pos, this->cvec.size()));
+                this->cvec.push_back(new_acc);
+        }
 }
 
 int Cache :: get(int pcno, char **buf)
