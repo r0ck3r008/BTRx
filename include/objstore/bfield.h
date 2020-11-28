8769775f30b56eb4f74b8f5c611469d5a5137c9c
r0ck3r008 <naman.arora@tutanota.com>
2020-11-28 10:51:34 -0500

    Seprated Bfield from Objstore with its own RWLock
            This comes from the realization that the node has to maintain
            its peers' bitfield too.

---
+++ include/objstore/bfield.h
@@ -0,0 +1,28 @@
+#ifndef BFIELD
+#define BFIELD
+
+#include<vector>
+#include<pthread.h>
+
+using std::vector;
+
+namespace objstore {
+        class Bfield {
+                vector<uint8_t> bfield;
+                pthread_rwlock_t rwlock;
+                bool needlock;
+        private:
+                void RdLock();
+                void WrLock();
+                void UnLock();
+        public:
+                Bfield(bool, bool, int);
+                ~Bfield();
+                void diff(vector<uint8_t>&,
+                                vector<uint8_t>&);
+                bool exists(int);
+                void flip(int);
+        };
+}
+
+#endif
---
+++ objstore/bfield.cpp
@@ -0,0 +1,124 @@
+#include<iostream>
+#include<string.h>
+#include<unistd.h>
+
+#include"objstore/bfield.h"
+#include"logger/logger.h"
+
+using objstore::Bfield;
+using logger::Logger;
+using logger::LogLvlT;
+
+extern Logger *lvar;
+
+Bfield :: Bfield(bool hasfile, bool needlock, int npcs)
+{
+	int wholes = npcs / 8;
+	int lftovr = npcs % 8;
+	this->bfield = vector<uint8_t>(wholes, (uint8_t)hasfile);
+        if(lftovr) {
+                if(hasfile) {
+                        uint8_t mask = 0;
+                        int max = (8 - lftovr);
+                        for(int i=7; i>=max; i--)
+                                mask ^= ((uint8_t)1<<i);
+                        this->bfield.push_back(mask);
+                } else {
+                        this->bfield.push_back((uint8_t)0);
+                }
+        }
+        this->needlock = needlock;
+        if(this->needlock) {
+                int stat = 0;
+                this->rwlock = PTHREAD_RWLOCK_INITIALIZER;
+                if((stat = pthread_rwlock_init(&(this->rwlock), NULL)) != 0) {
+                        lvar->write_msg(LogLvlT::LOG_DBG, "NODE: RWLock Init: %s",
+                                        strerror(stat));
+                        _exit(1);
+                }
+        }
+}
+
+Bfield :: ~Bfield()
+{
+        if(this->needlock) {
+                int stat=0;
+                if((stat = pthread_rwlock_destroy(&(this->rwlock))) != 0) {
+                        lvar->write_msg(LogLvlT::LOG_DBG, "NODE: RWLock Deinit: %s",
+                                        strerror(stat));
+                        _exit(1);
+                }
+        }
+}
+
+void Bfield :: RdLock()
+{
+        if(this->needlock) {
+                int stat = 0;
+                if((stat = pthread_rwlock_rdlock(&(this->rwlock))) != 0) {
+                        lvar->write_msg(LogLvlT::LOG_DBG, "NODE: RWLock RDLock: %s",
+                                        strerror(stat));
+                        _exit(1);
+                }
+        }
+}
+
+void Bfield :: WrLock()
+{
+        if(this->needlock) {
+                int stat = 0;
+                if((stat = pthread_rwlock_wrlock(&(this->rwlock))) != 0) {
+                        lvar->write_msg(LogLvlT::LOG_DBG, "NODE: RWLock WrLock: %s",
+                                        strerror(stat));
+                        _exit(1);
+                }
+        }
+}
+
+void Bfield :: UnLock()
+{
+        if(this->needlock) {
+                int stat = 0;
+                if((stat = pthread_rwlock_unlock(&(this->rwlock))) != 0) {
+                        lvar->write_msg(LogLvlT::LOG_DBG, "NODE: RWLock Unlock: %s",
+                                        strerror(stat));
+                        _exit(1);
+                }
+        }
+}
+
+void Bfield :: diff(vector<uint8_t>& right, vector<uint8_t>& diff)
+{
+        this->RdLock();
+	vector<uint8_t> left = this->bfield;
+	for(uint32_t i=0; i<left.size(); i++) {
+		uint8_t mask = left[i] ^ (left[i] | right[i]);
+		diff.push_back(mask);
+	}
+        this->UnLock();
+}
+
+/* Absolutely needs to be called with at least read lock set */
+bool Bfield :: exists(int pos)
+{
+	/* TODO: Make absolutely sure this method works */
+	int wholes = pos / 8;
+	int lftovr = pos % 8;
+	uint8_t mask = 1<<lftovr;
+        bool ret = false;
+
+	if(this->bfield[wholes] & mask)
+                ret=true;
+
+        return ret;
+}
+
+/* Absolutely needs to be called with write lock set */
+void Bfield :: flip(int pos)
+{
+	int wholes = pos / 8;
+	int lftovr = pos % 8;
+	uint8_t mask = 1<<lftovr;
+
+	this->bfield[wholes] ^= mask;
+}
