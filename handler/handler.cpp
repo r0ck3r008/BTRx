ff512e955c097a79d3ad84e86a2228c062ef7e5d
r0ck3r008 <naman.arora@tutanota.com>
2020-11-29 13:48:15 -0500

    Removing send/recv loops for a while
            This comes from the potential successful test of single
            threaded client handler.

--- handler/handler.cpp
+++ handler/handler.cpp
@@ -4,8 +4,6 @@
 #include<arpa/inet.h>
 
 #include"logger/logger.h"
-#include"handler/sendloop.h"
-#include"handler/recvloop.h"
 #include"handler/handler.h"
 
 using std::cout;
@@ -22,6 +20,5 @@ void cli_handler(int sock, struct sockaddr_in *addr, bool client)
                 cout << "Connection accepted from: ";
         else
                 cout << "Connected to: ";
-
         cout << inet_ntoa(addr->sin_addr) << endl;
 }
