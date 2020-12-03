3ac17cec1aa7ef0aa0a7619ca9393023f46318fb
r0ck3r008 <naman.arora@tutanota.com>
2020-12-03 14:15:12 -0500

    A script to automatically execute peers

---
+++ runner.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+peersid=($(awk '{print $1}' PeerInfo.cfg))
+peers=($(awk '{print $2}' PeerInfo.cfg))
+
+i=1
+
+while [ $i -le 9 ];
+do
+        ssh -t narora@"${peers["$i"]}" echo "${peersid["$i"]}"
+done
