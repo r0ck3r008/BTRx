461f57d1bdd2450a571ae7e1ca96e8fc077b56fb
r0ck3r008 <naman.arora@tutanota.com>
2020-12-03 14:27:51 -0500

    Bug: inifinite loop

--- runner.sh
+++ runner.sh
@@ -1,11 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
 peersid=($(awk '{print $1}' PeerInfo.cfg))
 peers=($(awk '{print $2}' PeerInfo.cfg))
 
 i=1
-
 while [ $i -le 9 ];
 do
         ssh -t narora@"${peers["$i"]}" echo "${peersid["$i"]}"
+        i=$(( $i + 1 ))
 done
