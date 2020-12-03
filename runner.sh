1ebf8c5e98541a799d3baf24e9bf822ee941628c
r0ck3r008 <naman.arora@tutanota.com>
2020-12-03 17:20:24 -0500

    Fix out of bounds error

--- runner.sh
+++ runner.sh
@@ -4,8 +4,8 @@ lines=$(wc -l PeerInfo.cfg | awk '{print $1}')
 peersid=($(awk '{print $1}' PeerInfo.cfg))
 peers=($(awk '{print $2}' PeerInfo.cfg))
 
-i=1
-while [ $i -le "$lines" ];
+i=0
+while [ $i -lt "$lines" ];
 do
         tmux new-session -d -s "${peersid["$i"]}" ssh -t narora@"${peers["$i"]}" htop
         i=$(( $i + 1 ))
