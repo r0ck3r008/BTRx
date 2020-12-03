5f9f4bdf2b60fdf9b9fdb14808110e406cd53e5d
r0ck3r008 <naman.arora@tutanota.com>
2020-12-03 14:39:08 -0500

    Added tmux as the session manager

--- runner.sh
+++ runner.sh
@@ -6,6 +6,6 @@ peers=($(awk '{print $2}' PeerInfo.cfg))
 i=1
 while [ $i -le 9 ];
 do
-        ssh -t narora@"${peers["$i"]}" echo "${peersid["$i"]}"
+        tmux new-session -d -s "${peersid["$i"]}" ssh -t narora@"${peers["$i"]}" htop
         i=$(( $i + 1 ))
 done
