d6c6e7505755d17dfa27c20158b6072696160311
r0ck3r008 <naman.arora@tutanota.com>
2020-12-03 14:42:13 -0500

    Get the number of peers from PeerInfo

--- runner.sh
+++ runner.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+lines=$(wc -l PeerInfo.cfg | awk '{print $1}')
 peersid=($(awk '{print $1}' PeerInfo.cfg))
 peers=($(awk '{print $2}' PeerInfo.cfg))
 
 i=1
-while [ $i -le 9 ];
+while [ $i -le "$lines" ];
 do
         tmux new-session -d -s "${peersid["$i"]}" ssh -t narora@"${peers["$i"]}" htop
         i=$(( $i + 1 ))
