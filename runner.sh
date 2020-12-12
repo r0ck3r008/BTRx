5e58360acb768804ea45497ca7736e8ce5108402
r0ck3r008 <naman.arora@tutanota.com>
2020-12-12 00:24:37 -0500

    Add real binary name

--- runner.sh
+++ runner.sh
@@ -7,6 +7,6 @@ peers=($(awk '{print $2}' PeerInfo.cfg))
 i=0
 while [ $i -lt "$lines" ];
 do
-        tmux new-session -d -s "${peersid["$i"]}" ssh -t narora@"${peers["$i"]}" htop
+        tmux new-session -d -s "${peersid["$i"]}" ssh -t narora@"${peers["$i"]}" ./peer "${peersid["$i"]}"
         i=$(( $i + 1 ))
 done
